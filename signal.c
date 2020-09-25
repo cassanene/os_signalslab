87756802e8b87bfed34b73042e3da77bc1f0b5ff
Cassey Anene <casseyanene@gmail.com>
2020-09-24 21:00:18 -0500

    Initial commit

---
+++ signal.c
@@ -0,0 +1,31 @@
+#include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+int global = 0;
+const int ALARM_CONST = 1;
+
+void handler(int signum)
+{ //signal handler
+    printf("Hello World!\n");
+    global = 1;
+    alarm(ALARM_CONST);
+}
+
+int main(int argc, char *argv[])
+{
+    signal(SIGALRM, handler); //register handler to handle SIGALRM
+    alarm(ALARM_CONST);       //schedule a SIGALRM for 1 second
+
+    while (1)
+    {
+        while (global == 0)
+            ; //infinite loop
+        printf("Turing was right!\n");
+        global = 0;
+        counter();
+        signal(SIGINT, sigint_handler);
+    };
+}
---
+++ timer.h
@@ -0,0 +1,8 @@
+#ifndef TIMER_H_
+#define TIMER_H_
+
+extern int seconds;
+void counter();
+void sigint_handler(int signum);
+
+#endif
